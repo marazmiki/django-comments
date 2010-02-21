ff95c1366cf027845d1cd87638b95b951339f067
unknown <root@.(none)>
2010-02-22 01:08:21 +0600

    настройка админки

--- admin/comment.py
+++ admin/comment.py
@@ -7,7 +7,12 @@ from django.utils.translation import ugettext as _
 from comments.models import Comment
 
 class CommentAdmin(admin.ModelAdmin):
-    pass
-    
+    fieldsets = [
+        (None, dict(fields=['content', 'parent_comment', 'is_approved'])),
+        (_('IP address'),     dict(fields=['remote_addr', 'forwarded_for',])),
+        (_('Date and time'),  dict(fields=['date_created', 'date_changed'])),
+        (_('Content object'), dict(fields=['content_type', 'object_pk'])),
+    ]
+
 admin.site.register(Comment, CommentAdmin)
 
