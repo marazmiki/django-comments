f87dd6059a658b57cfd39b6dd6343550c29da68a
unknown <root@.(none)>
2010-02-23 02:14:54 +0600

    тест юникода

--- tests/settings.py
+++ tests/settings.py
@@ -7,30 +7,35 @@ from comments.tests import get_content_object, create_comment
 
 class SettingsTest(TestCase):
     def setUp(self):
-        to_delete = CommentSettings.objects.all()
-        to_delete.delete()
-        
-        self.object = get_content_object()
-        self.comment = create_comment()
+        self.object   = get_content_object()
+        self.comment  = create_comment()
+        self.settings = CommentSettings.objects.create(
+            premoderate    = not PREMODERATE,
+            enabled        = not ENABLED,
+            level_limit    = LEVEL_LIMIT + 15,
+            content_object = self.object,
+        )
 
     def testInstance(self):
         self.assertTrue(isinstance(self.comment.get_settings(), CommentSettings))
 
     def testDefaultSettings(self):
+        to_delete = CommentSettings.objects.all()
+        to_delete.delete()
+
         comment_settings = self.comment.get_settings()
         
         self.assertEquals(PREMODERATE, comment_settings.premoderate)
         self.assertEquals(ENABLED,     comment_settings.enabled)
         self.assertEquals(LEVEL_LIMIT, comment_settings.level_limit)
 
-    def testOverrideSettings(self):
-        c = CommentSettings.objects.create(
-            premoderate    = not PREMODERATE,
-            enabled        = not ENABLED,
-            level_limit    = LEVEL_LIMIT + 15,
-            content_object = self.object,
+    def testUnicode(self):
+        self.assertEquals(
+            unicode(self.object),
+            unicode(self.comment.get_settings())
         )
-        c.save()
+
+    def testOverrideSettings(self):
 
         comment_settings = self.comment.get_settings()
 
