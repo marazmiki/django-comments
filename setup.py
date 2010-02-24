e44a12ddd490cd4bcba1f52770059bfe10641fa1
Michael Porokhovnichenko <marazmiki@gmail.com>
2010-02-24 12:49:42 +0600

    начинаем делать setup.py. Сейчас оно, конечно, не работает

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!env python
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(
+    name='Distutils',
+    version='1.0',
+    description='Python Distribution Utilities',
+    author='Greg Ward',
+    author_email='gward@python.net',
+    url ='http://www.python.org/sigs/distutils-sig/',
+    packages = ['comments', 'comments.admin', 'comments.templatetags', 'comments.tests',],
+)
+
